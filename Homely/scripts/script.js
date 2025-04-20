9c874b90debc31f016933b178892121d98673d23
shutdownmyeyez <148112295+shutdownmyeyez@users.noreply.github.com>
2025-04-20 16:29:18 +0600

    Delete Homely directory

--- Homely/scripts/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 100) {
-        header.style.background = 'rgba(34,34,34,0.95)';
-    } else {
-        header.style.background = 'rgba(34,34,34,0.9)';
-    }
-});
